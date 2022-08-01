29191d8f2b6e3325e73b17eefb186938ee5fa01e
wallace machado <wallacemachado@ntopus.com>
2022-08-01 17:27:04 -0300

    configurando fonte externa e add _document

---
+++ src/pages/_document.tsx
@@ -0,0 +1,22 @@
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
+
+// deve extender Document do next, ele só carrega uma vex na aplicação e toda vez que o usuário acessa 
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"/>
+          <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700;900&display=swap" rel="stylesheet"/>
+
+        </Head>
+        <body>
+          <Main />
+          <NextScript/> 
+        </body>
+      </Html> 
+    
+    )
+  }
+}
