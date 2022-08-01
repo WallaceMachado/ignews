1cb1531344425351f8af5d8c5c7b7a349b9a4c5a
wallace machado <wallacemachado@ntopus.com>
2022-08-01 17:44:29 -0300

    add estilos globas da app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 // AppProps -> propriedades que o componente pode receber
 import { AppProps } from "next/app";
+import "../styles/global.scss";
 
 // AppProps -> propriedades que o componente pode receber
 // App é carregado completamente toda vez que usuário troca de tela
