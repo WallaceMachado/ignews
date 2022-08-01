ca58dd8d2474f63d097de0c7637aa810bf782ecb
wallace machado <wallacemachado@ntopus.com>
2022-07-31 21:51:51 -0300

    instalando sass

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,10 @@
-import { AppProps } from "next/app"
+// AppProps -> propriedades que o componente pode receber
+import { AppProps } from "next/app";
 
+// AppProps -> propriedades que o componente pode receber
+// App é carregado completamente toda vez que usuário troca de tela
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
 export default MyApp
