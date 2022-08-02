8a0f0cd91eca6abece6b72781b7fc897f13c49d8
wallace machado <wallacemachado@ntopus.com>
2022-08-02 07:43:23 -0300

    add componentte header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 // AppProps -> propriedades que o componente pode receber
 import { AppProps } from "next/app";
 import "../styles/global.scss";
+import { Header } from '../components/Header';
 
 // AppProps -> propriedades que o componente pode receber
 // App é carregado completamente toda vez que usuário troca de tela
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Header />
+      <Component {...pageProps} />;
+    </>
+  )
+
 }
 
 export default MyApp
