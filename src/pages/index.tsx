b03d439162b65fbac65954336b3a456e14fda571
wallace machado <wallacemachado@ntopus.com>
2022-08-02 13:35:45 -0300

    add componente home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,7 @@
 import Head from "next/head";// tudo que for feita aqui vai ser refletido no head do _document
+import Image from "next/image";
+import avatarImg from "../../public/images/avatar.svg";
+import styles from "./home.module.scss";
 
 export default function Home() {
   return (
@@ -6,9 +9,19 @@ export default function Home() {
       <Head>
         <title>Início | ig.news</title>
       </Head>
-      <h1>
-        Hello World
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>
+            News about the <span>React</span> world.
+          </h1>
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+        <Image src={avatarImg} alt="Girl coding" />
+      </main>
     </>
   )
 }
