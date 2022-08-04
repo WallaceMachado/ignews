7501a6aafaa5db4592013881be127e37b4bb7aaf
wallace machado <wallacemachado@ntopus.com>
2022-08-04 14:13:04 -0300

    implementado qutenticação github com next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 // AppProps -> propriedades que o componente pode receber
 import { AppProps } from "next/app";
+import { SessionProvider as NextAuthProvider } from "next-auth/react"; // para que possa compartilhar dados doa sessao do usuario
 import "../styles/global.scss";
 import { Header } from '../components/Header';
 
@@ -7,11 +8,13 @@ import { Header } from '../components/Header';
 // App é carregado completamente toda vez que usuário troca de tela
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <>
+   
+    <NextAuthProvider session={pageProps.session}>
       <Header />
       <Component {...pageProps} />
-    </>
-  )
+    </NextAuthProvider>
+   
+  );
 
 }
 
