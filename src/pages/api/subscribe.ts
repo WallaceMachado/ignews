cab8597f2898530db62af22ce6bae0d52393b70b
wallace machado <wallacemachado@ntopus.com>
2022-08-05 17:32:05 -0300

    evitando duplicidade no stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -1,6 +1,19 @@
 import { NextApiRequest, NextApiResponse } from "next";
+import { query as q } from "faunadb";
 import { getSession } from "next-auth/react";
 import { stripe } from "../../services/stripe";
+
+import { fauna } from "../../services/fauna";
+import { query } from "faunadb";
+
+type User = {
+  ref: {
+    id: string;
+  };
+  data: {
+    stripe_customer_id: string;
+  };
+};
 
 // lugares seguros para gerar checkou session, como faz a partir do click do usuário melhor ser aqui
 // getServerSideProps (SSR)
@@ -19,14 +32,35 @@ export default async function subscribe(
     // backend consegue pegar sessão do usuário através de cookies (req), se tivesse salvo no localstorage não seria cessivel
     const session  = await req.body.user;
 
-    console.log("ijdishdjfhfui",session)
+    const user = await fauna.query<User>(
+        q.Get(q.Match(q.Index("user_by_email"), q.Casefold(session.email)))
+      );
+  
+      let customerId = user.data.stripe_customer_id;
+  
+      // se não existir
+      if (!customerId) {
+
     const stripeCustomer = await stripe.customers.create({
       email: session.email,
     });
 
+    // salva customer no banco
+      await fauna.query(
+        q.Update(q.Ref(q.Collection("users"), user.ref.id), {
+          data: {
+            stripe_customer_id: stripeCustomer.id,
+          },
+        })
+      );
+
+      // reatribui customer na variável
+      customerId = stripeCustomer.id;
+    }
+
     // informações para criação do checkout
     const stripeCheckoutSession = await stripe.checkout.sessions.create({
-      customer: stripeCustomer.id,
+        customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
       line_items: [{ price: "price_1LSQ14KmogR6ERdPXCqIdGar", quantity: 1 }],
